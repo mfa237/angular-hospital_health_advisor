3e4501809ecbb86411a6ad2c10d6e055f3e457ea
wanching97 <wanching_97@hotmail.com>
2018-05-10 21:37:13 +0800

    add waiting list as appointment

--- src/app/add-appointment/add-appointment.component.ts
+++ src/app/add-appointment/add-appointment.component.ts
@@ -4,8 +4,10 @@ import { AppointmentService } from '../appointment.service';
 import { Location } from '@angular/common';
 import { FormControl } from '@angular/forms';
 import { Doctor } from 'app/doctor';
+import { ActivatedRoute } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { WaitingList } from '../waiting_list';
 
 @Component({
   selector: 'app-add-appointment',
@@ -15,10 +17,13 @@ import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection 
 
 export class AddAppointmentComponent implements OnInit {
 
-  doctorColRef: AngularFirestoreCollection<Doctor>;
-  doctors: Doctor[] = [];
-  doctor: Doctor;
+  //doctorColRef: AngularFirestoreCollection<Doctor>;
+  //doctors: Doctor[] = [];
+  //doctor: Doctor;
+  waitingListDoc: AngularFirestoreDocument<WaitingList>;
+  waitingList: WaitingList;
   userId: string;
+  paramId = this.route.snapshot.paramMap.get('id');
 
   appointment: Appointment = {
     id: '',
@@ -36,7 +41,7 @@ export class AddAppointmentComponent implements OnInit {
     payment_status: false
   }
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private appointmentService: AppointmentService, private location: Location) {
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private route: ActivatedRoute, private appointmentService: AppointmentService, private location: Location) {
     afAuth.authState.subscribe( user => {
       if (user) {
         this.userId = user.uid;
@@ -44,33 +49,54 @@ export class AddAppointmentComponent implements OnInit {
     });
   }
    
-  ngOnInit(): void {
+  ngOnInit() {
     this.afAuth.authState.subscribe(d => console.log(d));
 
-    this.doctorColRef = this.afs.collection('doctors');
-    this.doctorColRef.ref.where('hospital_id', '==', this.userId).get().then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log('doc.id: ' + doc.id, '=>', doc.data());
+    this.waitingListDoc = this.afs.doc(`waiting_list/${this.paramId}`);
+    this.waitingListDoc.valueChanges().subscribe((listRef => {
+      if(listRef) {
+        this.waitingList = {
+          id: listRef.id,
+          appointment_desc: listRef.appointment_desc,
+          patient_name: listRef.patient_name,
+          doctor_name: listRef.doctor_name,
+          hospital_id: listRef.hospital_id,
+          date: listRef.date,
+          time: listRef.time
+        };
+      }
+    }))
 
-        this.doctors.push({
-          id : doc.get('id'),
-          name: doc.get('name'),
-          position: doc.get('position'),
-          ic: doc.get('ic'),
-          age: doc.get('age'),
-          email: doc.get('email'),
-          contact_number: doc.get('contact_number'),
-          hospital_id : doc.get('hospital_id')
-        });
+    // this.doctorColRef = this.afs.collection('doctors');
+    // this.doctorColRef.ref.where('hospital_id', '==', this.userId).get().then(snapshot => {
+    //   snapshot.forEach(doc => {
+    //     console.log('doc.id: ' + doc.id, '=>', doc.data());
 
-      });
-    }).catch(error => {
-      console.log(error);
-    })
+    //     this.doctors.push({
+    //       id : doc.get('id'),
+    //       name: doc.get('name'),
+    //       position: doc.get('position'),
+    //       ic: doc.get('ic'),
+    //       age: doc.get('age'),
+    //       email: doc.get('email'),
+    //       contact_number: doc.get('contact_number'),
+    //       hospital_id : doc.get('hospital_id')
+    //     });
+
+    //   });
+    // }).catch(error => {
+    //   console.log(error);
+    // })
 
   }
 
   onSubmit(){
+    this.appointment.description = this.waitingList.appointment_desc;
+    this.appointment.doctor_name = this.waitingList.doctor_name;
+    this.appointment.patient_name = this.waitingList.patient_name;
+    this.appointment.date = this.waitingList.date;
+    this.appointment.time = this.waitingList.time;
+    
     if(this.appointment.description != '' && this.appointment.patient_name != '' && this.appointment.doctor_name != null && this.appointment.date != '' && this.appointment.time != '') {
       console.log("the doctor is: " + this.appointment.doctor_name);
       this.appointmentService.addAppointment(this.appointment);
