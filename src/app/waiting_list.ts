4b593970adcfb9e95c709a5ecd766b2bfb2fa47b
wanching97 <wanching_97@hotmail.com>
2018-05-10 20:07:03 +0800

    changed waiting list attributes

--- src/app/waiting_list.ts
+++ src/app/waiting_list.ts
@@ -1,8 +1,7 @@
 export class WaitingList {
     id : string;
+    appointment_desc : string;
     patient_name : string;
-    patient_ic : string;
-    patient_sex : string;
     doctor_name : string;
     hospital_id : string;
     date : string;
